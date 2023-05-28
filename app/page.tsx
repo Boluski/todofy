549541e622babc614a4f2e4be6125eba6c7b29b8
Boluwatife Ajibola <ajibolss11@gmail.com>
2023-05-28 11:15:51 -0400

    add color scheme

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
 "use client";
 
 import React from "react";
+import { Stack, HStack, VStack, Box, Heading, Button } from "@chakra-ui/react";
 
-export default async function Page() {
+// sx={{ outline: "1px solid red" }
+export default function Page() {
   return (
     <>
-      <h1>Hello, Welcome to Todofy!</h1>
+      <Stack
+        direction={"row"}
+        justify={"space-between"}
+        align={"center"}
+        px={4}
+        py={2}
+        sx={{ outline: "1px solid red" }}
+      >
+        <Box>
+          <Heading size={"lg"} color={"brand.500"}>
+            Todofy
+          </Heading>
+        </Box>
+        <Stack direction={"row"}>
+          <Button colorScheme="brand">Sign Up</Button>
+          <Button variant={"outline"} colorScheme="brand">
+            Login
+          </Button>
+        </Stack>
+      </Stack>
     </>
   );
 }
