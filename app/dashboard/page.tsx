74a9ea11a2117dc6fc099af253202af5276ad9b7
Boluwatife Ajibola <ajibolss11@gmail.com>
2023-06-25 11:15:30 -0400

    Add user UID to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,27 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "../config";
+import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
+  const router = useRouter();
+  const auth = getAuth(app);
+  const [userId, setUserId] = useState("");
+
+  function toHome() {
+    router.push("/");
+  }
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setUserId(user.uid);
+    } else {
+      toHome();
+    }
+  });
   return (
     <>
-      <h1>Welcome to the dashboard</h1>
+      <h1>Welcome Back, {userId}</h1>
     </>
   );
 }
