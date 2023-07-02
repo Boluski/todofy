1a920b1fd3affea073740e13e7e552a8daefd4f1
Boluwatife Ajibola <ajibolss11@gmail.com>
2023-07-02 06:19:35 -0400

    add sign out functions

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,27 @@
 "use client";
 import React, { useState } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { app } from "../config";
 import { useRouter } from "next/navigation";
+import { Button } from "@chakra-ui/react";
 
 export default function Dashboard() {
   const router = useRouter();
   const auth = getAuth(app);
   const [userId, setUserId] = useState("");
 
+  // Redirects user to the landing page
   function toHome() {
     router.push("/");
+  }
+
+  // Signs out the current user
+  function signUserOut() {
+    signOut(auth)
+      .then(() => {})
+      .catch((error) => {
+        console.log(error);
+      });
   }
   onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -22,6 +33,14 @@ export default function Dashboard() {
   return (
     <>
       <h1>Welcome Back, {userId}</h1>
+      <Button
+        colorScheme="brand"
+        onClick={() => {
+          signUserOut();
+        }}
+      >
+        Sign Out
+      </Button>
     </>
   );
 }
